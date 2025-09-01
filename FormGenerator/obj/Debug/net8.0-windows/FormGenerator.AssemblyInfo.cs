475c09b9fdedf27ef07f96fb03d658a7cbbcfbe1
homepresso <andyhayes.nz@gmail.com>
2025-09-01 19:22:36 +0000

    ...

--- FormGenerator/obj/Debug/net8.0-windows/FormGenerator.AssemblyInfo.cs
+++ FormGenerator/obj/Debug/net8.0-windows/FormGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e8e0bbf1df2051d790bed3e97bdbeeafbecb38f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2db931988910ae424cbe504ceaefbfd43fdb347")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
